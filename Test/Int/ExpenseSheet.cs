765d0005536b53ca3998702a651414214265b923
David Salami <david.salami@camelotglobal.com>
2018-06-28 11:01:46 +0100

    filereader expense csv

---
+++ Test/Int/ExpenseSheet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Int
+{
+    public class ExpenseSheet
+    {
+        public string Id { get; set; }
+        public string Month { get; set; }
+        public string Date { get; set; }
+        public string Amount { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string DetailedCategory { get; set; }
+        public string Personnel { get; set; }
+        public string AdditionalDescription { get; set; }       
+        public string PaidBy { get; set; }
+        public string ExpenseId { get; set; }
+        public string Settled { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public int CategoryId { get; set; }
+        public decimal ExpenseAmount { get; set; }
+
+    }
+    
+    
+}
---
+++ Test/Int/ReadCsv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using Xunit;
+
+namespace Int
+{
+    public class ReadCsv
+    {
+        [Fact]
+        public void ReadTest()
+        {
+            var view =  Read("/tmp/expense.csv");
+            List<ExpenseSheet> result = new List<ExpenseSheet>();
+
+            foreach (var expense in view)
+            {
+                expense.ExpenseDate = DateTime.ParseExact(expense.Date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                expense.ExpenseAmount = decimal.Parse(string.Format("{0:N}", expense.Amount), System.Globalization.NumberStyles.Currency);
+                result.Add(expense);
+            }
+        }
+
+        public IEnumerable<ExpenseSheet>  Read(string path)
+        {
+
+            List<ExpenseSheet> result = new List<ExpenseSheet>();
+            using (TextReader fileReader = File.OpenText(path))
+            {
+                var csv = new CsvReader(fileReader);
+                csv.Configuration.HasHeaderRecord = true;
+                return csv.GetRecords<ExpenseSheet>().ToList();
+            }
+
+        }
+    }
+}
